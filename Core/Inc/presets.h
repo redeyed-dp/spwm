7bd47224e03c98fb53728910eebb0967616ce5b3
Yermakov Pavlo <redeyed.dp@gmail.com>
2025-04-04 12:43:14 +0300

    Feedback

--- Core/Inc/presets.h
+++ Core/Inc/presets.h
@@ -1,16 +1,24 @@
 #ifndef PRESETS_H
 #define PRESETS_H
 
-// Undervoltage protection 19V
+/*
+ * Battery settings
+ * undervoltage protection 19.0V * 107 = 2031
+ * undervoltage recovery   20.1V * 107 = 2151
+ * overvoltage recovery    25.5V * 107 = 2728
+ * overvoltage protection  26.5V * 107 = 2836
+ */
+
 #define UVP 2000
+#define UVR 2151
+#define OVR 2728
+#define OVP 2836
 
-// Undervoltage recovery 20.1V
-#define UVR 2200
-
-// Overvoltage recovery 25.5V
-#define OVR 2770
-
-// Overvoltage protection 26.5V
-#define OVP 2900
+/* Feedback
+ * 210V * 6.5909 = 1380
+ * 220V * 6.5909 = 1450
+ */
+#define FB_MIN 1380
+#define FB_MAX 1450
 
 #endif
